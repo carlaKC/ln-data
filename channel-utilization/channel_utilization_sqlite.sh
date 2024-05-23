cda67bc22d75e5a2b4e8630ea6d2fb3475534a2f
M1ch43l <100045502+M1ch43lV@users.noreply.github.com>
2024-05-23 20:49:36 +0200

    Update channel_utilization_sqlite.sh
    1. Perform sqlite3 integrity check of circuitbreaker.db
    2. Added resolved_time_ns

--- channel-utilization/channel_utilization_sqlite.sh
+++ channel-utilization/channel_utilization_sqlite.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Define the output CSV file
-output_file="temp.csv"
+output_file="forwarding_data_without_randomization.csv"
+
+# Perform integrity check on the SQLite database
+db_file="circuitbreaker.db"
+echo -n "Checking integrity of $db_file....."
+integrity_check=$(sqlite3 $db_file "PRAGMA integrity_check;")
+if [ "$integrity_check" != "ok" ]; then
+  echo "failed. Please stop circuitbreaker before copying db"
+  exit 1
+else
+  echo "ok"
+fi
 
 # Create CSV header
-echo "timestamp_ns,chan_id_in,chan_id_out,amt_in_msat,amt_out_msat,fee_msat" > "$output_file"
+echo "add_time_ns,resolved_time_ns,chan_id_in,chan_id_out,amt_in_msat,amt_out_msat,fee_msat" > "$output_file"
 
 # Initialize counter
 counter=0
-batch_size=10000
+batch_size=100000
 
 # Function to process a batch of data
 process_batch() {
     local offset=$1
     sqlite3 -csv -noheader circuitbreaker.db <<EOF
-SELECT add_time, incoming_channel, outgoing_channel, incoming_amt_msat, outgoing_amt_msat,
+SELECT add_time, resolved_time, incoming_channel, outgoing_channel, incoming_amt_msat, outgoing_amt_msat,
 (incoming_amt_msat - outgoing_amt_msat) AS fee_msat
 FROM forwarding_history
 LIMIT $batch_size OFFSET $offset;
@@ -39,14 +50,14 @@ while true; do
     echo "$batch_data" >> "$output_file"
 
     # Output status message
-    echo "Processed $counter rows"
+    echo "Processed $counter entries"
 
     # Increment offset for the next batch
     offset=$((offset + batch_size))
 done
 
-echo "Temporary CSV file created successfully: $output_file, randomizing data"
-../forwarding-history/randomize-data-fast.sh "$output_file"
+echo "Temporary CSV file created successfully: $output_file, now pseudo-randomizing channel_ids"
+./randomize-data-fast.sh "$output_file"
 
 #rm "$output_file"
-echo "Randomized forwarding history, final data available in forwarding_data.csv"
+echo "Channel IDs of forwarding history now randomized, final data available in forwarding_data.csv"
